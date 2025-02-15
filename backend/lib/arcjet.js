888a475f900242ec8e342417252fb3e58cb21717
Ata GÜNEŞER <ataguneser@hotmail.com>
2025-02-15 17:16:22 +0300

    Set up React Router and update App component structure

--- backend/lib/arcjet.js
+++ backend/lib/arcjet.js
@@ -1,21 +1,28 @@
-import arcjet, { tokenBucket, shield } from "arcjet";
+import arcjet, { tokenBucket, shield, detectBot, slidingWindow } from "@arcjet/node";
+
 import "dotenv/config";
 
+// init arcjet
 export const aj = arcjet({
     key: process.env.ARCJET_KEY,
     characteristics: ["ip.src"],
     rules: [
+        // shield protects your app from common attacks e.g. SQL injection, XSS, CSRF attacks
         shield({ mode: "LIVE" }),
-        detect({
-            mode: "LIVE", allow: [
-                "CATEGORY:SEARCH_ENGINE"
-            ]
+        detectBot({
+            mode: "LIVE",
+            // block all bots except search engines
+            allow: [
+                "CATEGORY:SEARCH_ENGINE",
+                // see the full list at https://arcjet.com/bot-list
+            ],
+        }),
+        // rate limiting
+        tokenBucket({
+            mode: "LIVE",
+            refillRate: 30,
+            interval: 5,
+            capacity: 20,
         }),
     ],
-    tokenBucket: {
-        mode: "LIVE",
-        refillRate: 5,
-        interval: 10,
-        capacity: 10
-    }
 });
